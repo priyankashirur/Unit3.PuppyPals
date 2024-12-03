499e0113d8db13b87a6b58dc00ce0d7a7499ea59
Priyanka Shirur <pshirur323@gmail.com>
2024-12-02 21:06:00 -0800

    featured pup

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,36 @@ import { puppyList } from "./data";
 function App() {
   const [count, setCount] = useState(0);
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setfeatPupId] = useState(null);
   console.log("array", puppies);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log("featured pup", featuredPup);
 
   return (
     <>
       <h1>Puppy Pals</h1>
       <div className="App">
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return (
+            <p
+              onClick={() => {
+                setfeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
         })}
+        {featuredPup && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
